ddf54bc6d9a665620dbf2c7ea3e6e408d5c5b482
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-03-20 07:14:35 +0000

    Lesson7-2-Progress

---
+++ lesson7-2.rb
@@ -0,0 +1,14 @@
+puts "計算を始めます"
+puts "何回繰り返しますか？"
+favorite = gets.to_i
+puts "1回目の計算"
+puts "2つの値を入力してくだい"
+a = gets.to_i
+b = gets.to_i
+puts "a=#{a}"
+puts "b=#{b}"
+puts "計算結果を出力します"
+puts "a+b=#{a+b}"
+puts "a-b=#{a-b}"
+puts "a*b=#{a*b}"
+puts "a/b=#{a/b}"
