1d9d6c879623c191a11317aa4ea92f6d2445b9e7
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-03-20 06:56:22 +0000

    Lesson7-1-Correct

--- lesson7-1.rb
+++ lesson7-1.rb
@@ -1,8 +1,7 @@
 puts "計算を始めます"
 puts "2つの値を入力してください"
-input_key1 = gets
-input_key2 = gets
+a = gets.to_i
+b = gets.to_i
 puts "計算結果を出力します"
-name = input_key1.to_i * input_key2.to_i
-puts "a*b=" + name.to_s
+puts "a*b=#{a*b}"
 puts "計算を終了します"
