0eda7793dff9acc28d357f23706b3294352bee4d
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-03-21 01:24:25 +0000

    Progress2

--- lesson7-2.rb
+++ lesson7-2.rb
@@ -1,14 +1,16 @@
 puts "計算を始めます"
 puts "何回繰り返しますか？"
 favorite = gets.to_i
+favorite.times do
 puts "1回目の計算"
 puts "2つの値を入力してくだい"
 a = gets.to_i
 b = gets.to_i
-puts "a=#{a}"
-puts "b=#{b}"
-puts "計算結果を出力します"
-puts "a+b=#{a+b}"
-puts "a-b=#{a-b}"
-puts "a*b=#{a*b}"
-puts "a/b=#{a/b}"
+  puts "a=#{a}"
+  puts "b=#{b}"
+  puts "計算結果を出力します"
+  puts "a+b=#{a+b}"
+  puts "a-b=#{a-b}"
+  puts "a*b=#{a*b}"
+  puts "a/b=#{a/b}"
+ end
